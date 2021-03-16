6e79eb357cd6a8b213e4d065f397f8c1d495bb9c
kaizer-rmk <mukesh.india9991@gmail.com>
2021-03-17 01:33:17 +0530

    upto Basic

---
+++ todo-functions.js
@@ -0,0 +1,63 @@
+//Read ToDos Data from localStorage
+const getSavedTodos = function(){
+    const todosJSON = localStorage.getItem('todos')
+    if(todosJSON !== null){
+        return JSON.parse(todosJSON)
+    }
+    else{
+        return []
+    }
+}
+
+//Save Todos to local storage
+const saveTodos = function(){
+    localStorage.setItem('todos',JSON.stringify(todos))
+}
+
+//Render Todos
+const renderTodos = function(todos,filters){
+
+    //Search
+    let filterTodos = todos.filter(function(todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    filterTodos = filterTodos.filter(function(todo){
+        if(filters.hideCompleted){
+            return !todo.completed
+        }
+        else{
+            return true
+        }
+    })
+
+    //Incomplete ToDo
+    const incompleteTodos = filterTodos.filter(function(todo){
+        return !todo.completed
+    })
+    //Clear todos Elements
+    document.querySelector('.todos').innerHTML=''
+    
+    //Summary
+    document.querySelector('.todos').appendChild(generateSummaryDOM(incompleteTodos))
+
+    //Taverse each todo
+    filterTodos.forEach(function(todo){
+        document.querySelector('.todos').appendChild(generateTodoDOM(todo))
+    })
+}
+
+//Generate Todo DOM
+const generateTodoDOM = function(todo){
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    return p
+}
+
+//Generate Summary
+const generateSummaryDOM = function(incompleteTodos){
+    //Display Summary
+    const summary = document.createElement('h3')
+    summary.textContent = `You have ${incompleteTodos.length} todos Left.`
+    return summary
+}
---
+++ todo.js
@@ -0,0 +1,39 @@
+let todos = getSavedTodos()
+
+const filters ={
+    searchText: '',
+    hideCompleted: false
+}
+
+renderTodos(todos,filters)
+
+//Search Input
+document.querySelector('#searchInput').addEventListener('input',function(e){
+    filters.searchText = e.target.value
+    renderTodos(todos,filters)
+})
+
+//Add Input from Form
+document.querySelector('#new-todo').addEventListener('submit',function(e){
+    e.preventDefault()
+    todos.push({
+        text:e.target.elements.addInput.value,
+        completed:false
+    })
+    saveTodos()
+    renderTodos(todos,filters)
+    e.target.elements.addInput.value=''
+})
+
+//Hide Completed
+document.querySelector('#hide-completed').addEventListener('change',function(e){
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos,filters)
+})
+
+document.querySelector('#filterBy').addEventListener('click',function(e){
+    console.log(e.target.value)
+})
+
+
+
